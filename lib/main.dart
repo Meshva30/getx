be5efc4beec93e23d197824acf80a86482906b46
Dell <5095meshwapatel@email.com>
2024-07-17 17:07:29 +0530

    17/7

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx/view/homescreen.dart';
+import 'package:getx/controller/theme_controller.dart';
+import 'package:getx/view/counterscreen.dart';
+import 'package:getx/view/navigationscreen.dart';
+import 'package:getx/view/themescreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeController themeController =  Get.put(ThemeController());
+   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Counterpage(),
+    return Obx(
+      () {
+        return   GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeController.isDarkMode.value?ThemeData.dark():ThemeData.light(),
+          // home: Themescreen(),
+
+          initialRoute: '/',
+            getPages: [
+              GetPage(
+                name: '/',
+                page: () => Navigationscreen(),
+              ),
+              GetPage(
+                  name: '/second',
+                  page: () => Secondscreen(),),
+            ],
+        );
+      }
 
     );
   }
